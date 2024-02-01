fe4ea58b8b2f6d02c7d94b7eaadfae71868515f7
Daniela Lopez <174199@mail.unacar.mx>
2024-02-01 01:57:56 -0600

    aleluya

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,38 @@
 // import SearchBar from "../components/SearchBar";
-
+import { useContext } from 'react'
+import ShowsContext from '../context/ShowsContext'
 import SearchBar from '../components/SearchBar'
+import ListItem from '../components/ListItem'
 
 const Home = () => {
+  const showsContext = useContext(ShowsContext)
+  const { loading, shows } = showsContext
+
   return (
     <div className='homepage'>
       <h1>Find your favourite shows to watch</h1>
       <SearchBar />
+      {loading
+        ? (
+          <h2>Waiting</h2>
+          )
+        : (
+          <div>
+            {shows.map((item) => (
+              <ListItem
+                key={item.show.id}
+                id={item.show.id}
+                image={item.show.image
+                  ? item.show.image.medium
+                  : 'https://fontawesome.com/icons/image?f=classic&s=duotone'}
+                name={item.show.name}
+                rating={item.show.rating.average
+                  ? item.show.rating.average
+                  : ''}
+              />
+            ))}
+          </div>
+          )}
     </div>
   )
 }
